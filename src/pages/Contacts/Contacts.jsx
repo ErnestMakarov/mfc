fa72fa9a1d7a0bfc4105ec8fca8111ba46e8fe9c
ErnestMakarov <ernest.makarov@tptlive.ee>
2026-08-16 02:28:50 +0300

    contact page add faq from home

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,12 @@
 import PageHero from "../../components/common/PageHero.jsx";
 
+import Faq from "../../components/sections/Faq.jsx";
+
 export default function Contacts() {
   return (
     <>
       <PageHero namespace="contacts" />
-
-      <main>
-        Contacts content
-      </main>
+      <Faq />
     </>
   );
 }
